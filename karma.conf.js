573f56b44c2ff2baf7776497dfd1f1f883a476fe
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-08-08 14:17:34 +0200

    Fix build errors

--- karma.conf.js
+++ karma.conf.js
@@ -4,81 +4,81 @@
 // generator-karma 1.0.0
 
 module.exports = function (config) {
-    'use strict';
+  'use strict';
 
-    config.set({
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+  config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
-        // base path, that will be used to resolve files and exclude
-        basePath: '.',
+    // base path, that will be used to resolve files and exclude
+    basePath: '.',
 
-        // testing framework to use (jasmine/mocha/qunit/...)
-        // as well as any additional frameworks (requirejs/chai/sinon/...)
-        frameworks: [
-            "jasmine"
-        ],
+    // testing framework to use (jasmine/mocha/qunit/...)
+    // as well as any additional frameworks (requirejs/chai/sinon/...)
+    frameworks: [
+      "jasmine"
+    ],
 
-        // list of files / patterns to load in the browser
-        files: [
-            'bower_components/angular/angular.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            "src/**/*.js",
-            "test/app/**/*.js",
-            "test/mocks/**/*.js",
-            "test/specs/**/*.js"
-        ],
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      "src/**/*.js",
+      "test/app/**/*.js",
+      "test/mocks/**/*.js",
+      "test/specs/**/*.js"
+    ],
 
-        // list of files / patterns to exclude
-        exclude: [],
+    // list of files / patterns to exclude
+    exclude: [],
 
-        // web server port
-        port: 9080,
+    // web server port
+    port: 9080,
 
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera
-        // - Safari (only Mac)
-        // - PhantomJS
-        // - IE (only Windows)
-        browsers: [
-            "PhantomJS"
-        ],
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: [
+      "PhantomJS"
+    ],
 
-        // Which plugins to enable
-        plugins: [
-            "karma-phantomjs-launcher",
-            "karma-jasmine",
-            "karma-junit-reporter"
-        ],
+    // Which plugins to enable
+    plugins: [
+      "karma-phantomjs-launcher",
+      "karma-jasmine",
+      "karma-junit-reporter"
+    ],
 
-        reporters: [
-            "progress",
-            "junit"
-        ],
+    reporters: [
+      "progress",
+      "junit"
+    ],
 
-        junitReporter: {
-            outputDir: 'dist/test-report',// results will be saved as $outputDir/$browserName.xml
-            suite: 'angular-errorhandler'
-        },
+    junitReporter: {
+      outputDir: 'dist/test-report',// results will be saved as $outputDir/$browserName.xml
+      suite: 'angular-invocation-handler'
+    },
 
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: false,
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
 
-        colors: true,
+    colors: true,
 
-        // level of logging
-        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-        logLevel: config.LOG_INFO,
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-        // Uncomment the following lines if you are using grunt's server to run the tests
-        // proxies: {
-        //   '/': 'http://localhost:9000/'
-        // },
-        // URL root prevent conflicts with the site root
-        // urlRoot: '_karma_'
-    });
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies: {
+    //   '/': 'http://localhost:9000/'
+    // },
+    // URL root prevent conflicts with the site root
+    // urlRoot: '_karma_'
+  });
 };
